0acba8feae4c40cb8a235adf94a2c54260cda11a
Kapil <kapilgargymca@gmail.com>
2024-03-09 19:52:19 +0530

    ist branch

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]) {
+        RWOnly kk = new RWOnly();
+        //kk.name = "kapil";
+        //java: name has private access in com.driver.RWOnly
+
+        kk.setName("kapil");
+        kk.getName();
+    }
+
 }
